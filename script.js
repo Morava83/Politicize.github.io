89732f521867467f4cc0fad5c6927e1d84e1eae6
Brian Morava <zmorava515@hotmail.com>
2023-04-05 01:33:51 -0400

    all presidents

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function politicize() {
     var input = document.getElementById("input").value.toLowerCase();
     var image = document.getElementById("image");
-    if (input === "donald trump") {
+    const republicans = ["abraham lincoln", "ulysses s. grant", "ulysses grant", "rutherford b. hayes", "rutherford hayes", "james a. garfield", "james garfield", "chester a. arthur", "chester arthur", "benjamin harrison", "william mckinley", "theodore roosevelt", "william howard taft", "william taft", "warren g. harding", "warren harding", "calvin coolidge", "herbert hoover", "dwight d. eisenhower", "dwight eisenhower", "eisenhower", "richard nixon", "gerald ford", "ronald reagan", "george h.w. bush", "george bush", "george w. bush", "donald j. trump", "donald trump", "trump"];
+    const democrats = ["andrew jackson", "martin van buren", "martin buren", "james k. polk", "james polk", "franklin pierce", "james buchanan", "andrew johnson", "grover cleveland", "woodrow wilson", "franklin d. roosevelt", "franklin roosevelt", "fdr", "harry s. truman", "harry truman", "john f. kennedy", "john kennedy", "jfk", "lyndon b. johnson", "lyndon johnson", "james carter", "jimmy carter", "william clinton", "bill clinton", "barack obama", "joe biden", "biden"];
+    if (republicans.includes(input)) {
       image.src = "https://upload.wikimedia.org/wikipedia/commons/9/9b/Republicanlogo.svg";
-    } else if (input === "joe biden") {
+    } else if (democrats.includes(input)) {
       image.src = "https://img.freepik.com/premium-vector/donkey-colors-american-flag-symbol-democratic-party-usa-isolated-icon-vector-illustration-design_431724-7343.jpg";
     } else {
       alert("Invalid input.");
