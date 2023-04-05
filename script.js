4ce285e8a971cd23c35e0c1972554cd710ffc93c
Brian Morava <zmorava515@hotmail.com>
2023-04-05 00:55:07 -0400

    no photo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function politicize() {
     if (input === "donald trump") {
       image.src = "https://upload.wikimedia.org/wikipedia/commons/9/9b/Republicanlogo.svg";
     } else if (input === "joe biden") {
-      image.src = "https://img.freepik.com/premium-vector/donkey-colors-american-flag-symbol-democratic-party-usa-isolated-icon-vector-illustration-design_431724-7343.jpg";
+      image.src = "";
     } else {
       alert("Invalid input.");
     }
