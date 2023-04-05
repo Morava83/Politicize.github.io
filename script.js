6f00184ec55d9fb58578d868892545911aa9dd8a
Brian Morava <zmorava515@hotmail.com>
2023-04-04 23:38:56 -0400

    interactive-V0

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function politicize() {
-    document.body.appendChild(republicanImage);
-    const inputText = document.getElementById("input-text").value;
-    if (inputText.toLowerCase() === "donald trump") {
-      document.body.innerHTML = "";
-      const republicanImage = document.createElement("img");
-      republicanImage.src = "https://www.worldatlas.com/r/w1200/upload/cf/5e/28/untitled-design-22.jpg";
-      document.body.appendChild(republicanImage);
-    } else if (inputText.toLowerCase() === "joe biden") {
-      document.body.innerHTML = "";
-      const democratImage = document.createElement("img");
-      democratImage.src = "https://www.worldatlas.com/r/w1200/upload/ea/c3/c5/shutterstock-319420406.jpg";
-      document.body.appendChild(democratImage);
+    var input = document.getElementById("input").value.toLowerCase();
+    var image = document.getElementById("image");
+    if (input === "donald trump") {
+      image.src = "https://www.worldatlas.com/r/w1200/upload/cf/5e/28/untitled-design-22.jpg";
+    } else if (input === "joe biden") {
+      image.src = "https://www.worldatlas.com/r/w1200/upload/ea/c3/c5/shutterstock-319420406.jpg";
+    } else {
+      alert("Invalid input.");
     }
+    document.getElementById("input").value = "";
   }
-  
-  const politicizeButton = document.getElementById("politicize-button");
-  politicizeButton.addEventListener("click", politicize);
-  
