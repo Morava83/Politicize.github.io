957b1b26489256b3dc2a3f0f5362f9918e91a023
Brian Morava <zmorava515@hotmail.com>
2023-05-01 18:42:41 -0400

    test alert

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function politicize() {
-    var input = document.getElementById("input").value.toLowerCase();
+  alert("hi");
+  /*  
+  var input = document.getElementById("input").value.toLowerCase();
     var image = document.getElementById("image");
-    //const republicans = ["abraham lincoln", "ulysses s. grant", "ulysses grant", "rutherford b. hayes", "rutherford hayes", "james a. garfield", "james garfield", "chester a. arthur", "chester arthur", "benjamin harrison", "william mckinley", "theodore roosevelt", "william howard taft", "william taft", "warren g. harding", "warren harding", "calvin coolidge", "herbert hoover", "dwight d. eisenhower", "dwight eisenhower", "eisenhower", "richard nixon", "gerald ford", "ronald reagan", "george h.w. bush", "george bush", "george w. bush", "donald j. trump", "donald trump", "trump"];
-    //const democrats = ["andrew jackson", "martin van buren", "martin buren", "james k. polk", "james polk", "franklin pierce", "james buchanan", "andrew johnson", "grover cleveland", "woodrow wilson", "franklin d. roosevelt", "franklin roosevelt", "fdr", "harry s. truman", "harry truman", "john f. kennedy", "john kennedy", "jfk", "lyndon b. johnson", "lyndon johnson", "james carter", "jimmy carter", "william clinton", "bill clinton", "barack obama", "joe biden", "biden"];
+    
     //questions taken from: https://www.pewresearch.org/
     alert("hi");
     var Q1 = prompt("You would rather have a smaller government providing fewer services than a bigger government providing more services"); //R
@@ -69,5 +70,6 @@ function politicize() {
     }
     
     document.getElementById("input").value = "";
+    */
   }
   
