8618eff861cd97c3e66a6fa5389b5433996a839b
Brian Morava <zmorava515@hotmail.com>
2023-05-01 16:44:00 -0400

    test

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function politicize() {
     const republicans = ["abraham lincoln", "ulysses s. grant", "ulysses grant", "rutherford b. hayes", "rutherford hayes", "james a. garfield", "james garfield", "chester a. arthur", "chester arthur", "benjamin harrison", "william mckinley", "theodore roosevelt", "william howard taft", "william taft", "warren g. harding", "warren harding", "calvin coolidge", "herbert hoover", "dwight d. eisenhower", "dwight eisenhower", "eisenhower", "richard nixon", "gerald ford", "ronald reagan", "george h.w. bush", "george bush", "george w. bush", "donald j. trump", "donald trump", "trump"];
     const democrats = ["andrew jackson", "martin van buren", "martin buren", "james k. polk", "james polk", "franklin pierce", "james buchanan", "andrew johnson", "grover cleveland", "woodrow wilson", "franklin d. roosevelt", "franklin roosevelt", "fdr", "harry s. truman", "harry truman", "john f. kennedy", "john kennedy", "jfk", "lyndon b. johnson", "lyndon johnson", "james carter", "jimmy carter", "william clinton", "bill clinton", "barack obama", "joe biden", "biden"];
     var name = prompt("Please enter your name:", "John Doe");
+    /* 
     if (republicans.includes(input)) {
       image.src = "https://upload.wikimedia.org/wikipedia/commons/9/9b/Republicanlogo.svg";
     } else if (democrats.includes(input)) {
@@ -11,6 +12,7 @@ function politicize() {
     } else {
       alert("Invalid input.");
     }
+    */
     document.getElementById("input").value = "";
   }
   
