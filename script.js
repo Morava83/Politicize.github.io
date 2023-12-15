9f20164466fb11e13fa2e533ef33cde731e928b2
Brian Morava <84289395+Morava83@users.noreply.github.com>
2023-12-14 21:59:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function politicize() {
   //questions adapted from: https://www.pewresearch.org/
   
   var Q9 = prompt("I don't consider it a problem when people are "triggered" by something someone says\nAnswer with: yes/maybe/no"); //R
-  var Q10 = prompt("People saying things that are very offensive to others is a major problem\nAnswer with: yes/maybe/no"); //D
+  var Q10 = prompt("Overall, people who are convicted of crimes in this country serve too much time in prison\nAnswer with: yes/maybe/no"); //D
   var Q11 = prompt("When it comes to candidates for political office, I usually feel like there is at least one candidate who shares most of my views\nAnswer with: yes/maybe/no"); //R
   var Q12 = prompt("In general, White people benefit a fair amount from advantages in society that Black people do not have\nAnswer with: yes/maybe/no"); //D
   var Q13 = prompt("Greater social acceptance of people who are transgender is somewhat bad for society\nAnswer with: yes/maybe/no"); //R
-  var Q14 = prompt("Overall, people who are convicted of crimes in this country serve too much time in prison\nAnswer with: yes/maybe/no"); //D
+  var Q14 = prompt("People saying things that are offensive to others is a major problem\nAnswer with: yes/maybe/no"); //D
   var Q15 = prompt("Government policies should support religious views and beliefs\nAnswer with: yes/maybe/no"); //R
   var Q16 = prompt("It would be acceptable if another country became as militarily powerful as the U.S\nAnswer with: yes/maybe/no"); //D
   var Q17 = prompt("On a scale of 0 to 100, where 0 means you feel as cold and negative as possible and 100 means you feel as warm and positive as possible\nHow do you feel toward Democrats?");
